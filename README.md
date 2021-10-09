f54c668ba3c495f51de07cd8ac72de1a62182cd2
Hansani926 <hansanisewwandi926@gmail.com>
2021-10-09 09:10:18 +0530

    Initalize Project

---
+++ README.md
@@ -0,0 +1,10 @@
+This project is GITEXAM project
+
+app run
+ setup gradle and run
+ 
+app build
+  gradle build
+  
+deployment instructions
+ build and get jar then deploy 
---
+++ src/AppInitializer.java
@@ -0,0 +1,32 @@
+/*
+ *
+ *  * ---------------------------------------------------------------------------------------------
+ *  *  *  Copyright (c) GDSE-56-GitExam. All rights reserved.
+ *  *  *  Licensed under the SriLankan Information License. See License.txt in the project root for license information.
+ *  *  *--------------------------------------------------------------------------------------------
+ *
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * @author : Hansani Sewwandi
+ * @since 10/9/2021
+ **/
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/Customer.fxml"))));
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
